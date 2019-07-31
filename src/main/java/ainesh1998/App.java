f2bebd63128df319f93ef7ddf7947404c8818d6b
ainesh1998 <ainesh1998@outlook.com>
2019-07-31 08:02:41 +0800

    initial version

---
+++ src/main/java/ainesh1998/App.java
@@ -0,0 +1,13 @@
+package ainesh1998;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
